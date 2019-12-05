acc58dc10b1effaca6d8877a52b5e8afbdbb7815
Crowdbotics <team@crowdbotics.com>
2019-12-05 17:05:08 +0000

    Update models

--- django_new_backend_7_1128/wsgi.py
+++ django_new_backend_7_1128/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_new_backend_7_1128.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_new_backend_7_1128.settings")
 
 application = get_wsgi_application()
--- home/models.py
+++ home/models.py
@@ -6,18 +6,21 @@ from django.db import models
 
 
 class CustomText(models.Model):
-    title = models.CharField(max_length=150)
+    title = models.CharField(max_length=150,)
+    r2 = models.ManyToManyField(
+        "home.HomePage", blank=True, related_name="customtext_r2",
+    )
 
     def __str__(self):
         return self.title
 
     @property
     def api(self):
-        return f'/api/v1/customtext/{self.id}/'
+        return f"/api/v1/customtext/{self.id}/"
 
     @property
     def field(self):
-        return 'title'
+        return "title"
 
 
 class HomePage(models.Model):
@@ -25,8 +28,8 @@ class HomePage(models.Model):
 
     @property
     def api(self):
-        return f'/api/v1/homepage/{self.id}/'
+        return f"/api/v1/homepage/{self.id}/"
 
     @property
     def field(self):
-        return 'body'
+        return "body"
